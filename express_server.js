a6c9d3fcb09ad8f54f656db5df532aff79614a2e
kamiada <kamiada528@gmail.com>
2021-04-18 12:58:25 +0100

    works on postman

--- express_server.js
+++ express_server.js
@@ -8,5 +8,12 @@ app.get('/octopus', (req, res) => {
     //every time when this endpoint is requested - localhost:8080/octopus - run the function below
     //request (req) - incoming data
     //response (res) - outcoming data
-    res.status(200);
+    res.status(200).send({
+        emoji: '🐙',
+        funFact: 'Octopuses arms have mind of their own'
+    });
+})
+
+app.post('/octopus/:id', (req, res) => {
+    const {id} = req.params;
 })
