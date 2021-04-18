d5e8da7c11541b734cba8246452c07a284d32030
kamiada <kamiada528@gmail.com>
2021-04-18 14:15:06 +0100

    posting

--- express_server.js
+++ express_server.js
@@ -1,23 +1,23 @@
-const app = require('express')();
+const app = require("express")();
 const PORT = 8080;
-app.listen(
-    PORT,
-    () => console.log(`hosted on http://localhost:${PORT}`)
-)
-app.get('/octopus', (req, res) => {
-    //every time when this endpoint is requested - localhost:8080/octopus - run the function below
-    //request (req) - incoming data
-    //response (res) - outcoming data
-    res.status(200).send({
-        emoji: '🐙',
-        funFact: 'Octopuses arms have mind of their own'
-    });
-})
+app.listen(PORT, () => console.log(`hosted on http://localhost:${PORT}`));
+app.get("/octopus", (req, res) => {
+  //every time when this endpoint is requested - localhost:8080/octopus - run the function below
+  //request (req) - incoming data
+  //response (res) - outcoming data
+  res.status(200).send({
+    octopus: "🐙",
+    funFact: "Octopuses arms have mind of their own",
+  });
+});
 
-app.post('/octopus/:id', (req, res) => {
-    const {id} = req.params;
-    const logo = req.body;
-    if(!logo) {
-        res.status(418).send({message: "We need a logo!"})
-    }
-})
+app.post("/octopus/:id", (req, res) => {
+  const { id } = req.params;
+  const logo = req.body; //important to know - express doesn't parse json in the body by default - middleware needs to be set up to parse data into json before the function
+  if (!logo) {
+    res.status(418).send({ message: "We need a logo!" });
+  }
+  res.send({
+    octopus: `🐙 with your ${logo} and ID of ${id}`,
+  });
+});
