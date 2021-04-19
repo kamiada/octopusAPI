995326d583d639c1117cdba8200395e1c7b2b6f6
kamiada <kamiada528@gmail.com>
2021-04-20 00:16:16 +0100

    nanan batman

---
+++ octopus_web_app/src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./style.scss";
+
+class Button extends Component {
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+export default Button;
