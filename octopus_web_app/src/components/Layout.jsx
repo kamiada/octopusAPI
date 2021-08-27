f3bcd748075244aa956763bec62943e548faca93
kamiada <kamiada528@gmail.com>
2021-08-27 18:29:10 +0100

    A bit more

---
+++ octopus_web_app/src/components/Layout.jsx
@@ -0,0 +1,11 @@
+import "./style.scss";
+
+
+const Layout = ({ children }) => {
+    return(
+        <div className="layout">
+            <main>{children}</main>
+        </div>
+    )
+}
+export default Layout;
---
+++ octopus_web_app/src/components/index.js
@@ -0,0 +1,4 @@
+
+import Layout from './Layout.jsx';
+import Button from './Button.jsx';
+export { Layout, Button };
